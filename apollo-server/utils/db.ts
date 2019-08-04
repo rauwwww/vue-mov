0b680083193d0819de4e4d6feb45fbf10881a4a7
Someone <Patrickholsths@gmail.com>
2019-08-04 11:07:20 +0200

    types: lowdb + mkdirp

--- apollo-server/utils/db.ts
+++ apollo-server/utils/db.ts
@@ -1,14 +1,20 @@
-import Lowdb from 'lowdb'
-import FileSync from 'lowdb/adapters/FileSync'
-import mkdirp from 'mkdirp'
-import { resolve } from 'path'
+import Lowdb from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+import mkdirp from 'mkdirp';
 
-mkdirp(resolve(__dirname, '../../live'))
+// mkdirp(resolve(__dirname, '../../live'));
+mkdirp('../../live', (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('success');
+});
 
-export const db = new Lowdb(new FileSync(resolve(__dirname, '../../live/db.json')))
+const adapter = new FileSync('../../live/db.json');
+export const db = Lowdb(adapter);
 
 // Seed an empty DB
 db.defaults({
   messages: [],
-  uploads: [],
-}).write()
+  uploads: []
+}).write();
