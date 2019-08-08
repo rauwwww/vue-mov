69991b524e7c7f2d362ad5a000768780b8f1e4dd
Someone <Patrickholsths@gmail.com>
2019-08-09 00:07:32 +0200

    wip: oemdb helper

--- src/store/movies/_actions/fetchMovies.ts
+++ src/store/movies/_actions/fetchMovies.ts
@@ -2,18 +2,22 @@ import { ActionContext } from 'vuex';
 import MoviesState from '../movies.state';
 import RootState from '../../state';
 import { oemdbReq } from '@/common/oemdbApiHelper';
-import { IOemdbReq, IOemdbReqPrefixes } from '../movies.types';
+import { IOemdbReq, IOemdbReqPrefix } from '../movies.types';
 
-export default async function fetchMovies({  }: ActionContext<MoviesState, RootState>, title: any): Promise<any> {
-  const tempDataArr: any[] = [];
+export default async function fetchMovies({  }: ActionContext<MoviesState, RootState>, title: string): Promise<any> {
+  // const tempDataArr: any[] = [];
   const reqEx = new IOemdbReq();
-  const apiQuery = reqEx.reqParam(IOemdbReqPrefixes.movieTitle, title);
-  console.log(reqEx.reqParam(IOemdbReqPrefixes.movieTitle, title));
+  const payload: IOemdbReqPrefix = {
+    movieTitle: title
+  };
+  // const apiQuery = '';
+  const apiQuery = reqEx.generateReqParams(payload);
+  console.log(reqEx.generateReqParams(payload));
   // `t=${title}`;
   try {
     await oemdbReq(apiQuery).then((res) => {
-      console.log(res);
-      console.log(tempDataArr);
+      // console.log(res);
+      // console.log(tempDataArr);
     });
 
     return 'loaded cases';
--- src/store/movies/movies.types.ts
+++ src/store/movies/movies.types.ts
@@ -11,47 +11,59 @@ export interface IMovies {
 // }
 
 export class IOemdbReq {
-  imdbId: string;
-  movieTitle: string;
+  private apiReqTypes: IOemdbReqPrefixes = {
+    movieTitle: '?t=',
+    imdbId: '?i='
+  };
+  // imdbId: string;
+  // movieTitle: string;
   // type,
   // releaseYear,
   // plot,
   // dataReturnType,
   // page
 
-  constructor() {
-    this.imdbId = `?i=`;
-    this.movieTitle = `?t=`;
-    //   this.reqPrefixes.movieTitle = 't';
-    //   (this.reqPrefixes.type = {
-    //     type: 'type',
-    //     typeOf: 'movies'
-    //   }),
-    //     (this.reqPrefixes.releaseYear = 'y'),
-    //     (this.reqPrefixes.plot = 'plot'),
-    //     (this.reqPrefixes.dataReturnType = 'json'),
-    //     (this.reqPrefixes.page = 10);
-    // }
-  }
+  // constructor() {
+  //   this.imdbId = `?i=`;
+  //   this.movieTitle = `?t=`;
+  //   //   this.reqPrefixes.movieTitle = 't';
+  //   //   (this.reqPrefixes.type = {
+  //   //     type: 'type',
+  //   //     typeOf: 'movies'
+  //   //   }),
+  //   //     (this.reqPrefixes.releaseYear = 'y'),
+  //   //     (this.reqPrefixes.plot = 'plot'),
+  //   //     (this.reqPrefixes.dataReturnType = 'json'),
+  //   //     (this.reqPrefixes.page = 10);
+  //   // }
+  // }
 
-  /**
-   *
-   * @param type movieTitle, imdbId, releaseYear, plot
-   * @param searchString
-   */
-  reqParam(type: IOemdbReqPrefixes, searchString: string) {
-    switch (type) {
-      case 'movieTitle': {
-        return this.movieTitle + searchString;
-      }
-      case 'imdbId': {
-        return this.imdbId + searchString;
-      }
-      default: {
-        return this.imdbId + searchString;
+  public generateReqParams(payload: IOemdbReqPrefix) {
+    // return this.apiReqTypes.keys((el: IOemdbReqPrefix) => el.movieTitle === payload);
+    if (payload) {
+      if (payload.movieTitle) {
+        return this.apiReqTypes.movieTitle + payload.movieTitle;
       }
     }
   }
+  // /**
+  //  *
+  //  * @param type movieTitle, imdbId, releaseYear, plot
+  //  * @param searchString
+  //  */
+  // reqParam(type: IOemdbReqPrefixes, searchString: string) {
+  //   switch (type) {
+  //     case 'movieTitle': {
+  //       return this.movieTitle + searchString;
+  //     }
+  //     case 'imdbId': {
+  //       return this.imdbId + searchString;
+  //     }
+  //     default: {
+  //       return this.imdbId + searchString;
+  //     }
+  //   }
+  // }
 }
 
 /**
@@ -68,11 +80,11 @@ export interface IOemdbReqPrefix {
 }
 
 export enum IOemdbReqPrefixes {
-  imdbId = 'imdbId',
-  movieTitle = 'movieTitle',
+  imdbId = '?i=',
+  movieTitle = '?t=',
   // type?: IOemdbReqTypes;
-  releaseYear = 'releaseYear',
-  plot = 'plot'
+  releaseYear = '?y=',
+  plot = '?plot='
   // dataReturnType = 'json' | 'xml',
   // page ?: number;
 }
