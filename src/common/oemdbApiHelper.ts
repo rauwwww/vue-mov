9a79bb38b7c6db9cbce83ed29a500ed2a2aafcba
Someone <Patrickholsths@gmail.com>
2019-08-09 13:23:32 +0200

    refactor: promise --> await async

--- src/common/oemdbApiHelper.ts
+++ src/common/oemdbApiHelper.ts
@@ -4,29 +4,7 @@ const oemdbBaseUrl = 'http://www.omdbapi.com/?apikey=57472c69&';
 
 export async function oemdbReq(params?: any) {
   console.log(oemdbBaseUrl + params);
-
-  axios
-    .request<ServerData>({
-      url: oemdbBaseUrl + params,
-      transformResponse: (r: ServerResponse) => r.data
-    })
-    .then((response) => {
-      // `response` is of type `AxiosResponse<ServerData>`
-      const data = response;
-      console.log(data);
-      // `data` is of type ServerData, correctly inferred
-      return data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-interface ServerResponse {
-  data: ServerData;
-}
-
-interface ServerData {
-  foo: string;
-  bar: number;
+  const res = await axios.get(oemdbBaseUrl + params);
+  console.log(res);
+  return res;
 }
