d36035da64f0911ef1700551754a6aba1d469499
Someone <Patrickholsths@gmail.com>
2019-08-11 20:43:39 +0200

    refactor: linting apollo scope

--- apollo-server/resolvers.ts
+++ apollo-server/resolvers.ts
@@ -1,72 +1,69 @@
-import GraphQLJSON from 'graphql-type-json'
-import shortid from 'shortid'
-
+import GraphQLJSON from 'graphql-type-json';
+import shortid from 'shortid';
 
 export default {
   JSON: GraphQLJSON,
 
   Counter: {
-    countStr: counter => `Current count: ${counter.count}`,
+    countStr: (counter) => `Current count: ${counter.count}`
   },
-
 
   Query: {
     hello: (root, { name }) => `Hello ${name || 'World'}!`,
     messages: (root, args, { db }) => db.get('messages').value(),
-    uploads: (root, args, { db }) => db.get('uploads').value(),
-
+    uploads: (root, args, { db }) => db.get('uploads').value()
   },
 
   Mutation: {
     myMutation: (root, args, context) => {
-      const message = 'My mutation completed!'
-      context.pubsub.publish('hey', { mySub: message })
-      return message
+      const message = 'My mutation completed!';
+      context.pubsub.publish('hey', { mySub: message });
+      return message;
     },
     addMessage: (root, { input }, { pubsub, db }) => {
       const message = {
         id: shortid.generate(),
-        text: input.text,
-      }
+        text: input.text
+      };
 
-      db
-        .get('messages')
+      db.get('messages')
         .push(message)
         .last()
-        .write()
+        .write();
 
-      pubsub.publish('messages', { messageAdded: message })
+      pubsub.publish('messages', { messageAdded: message });
 
-      return message
+      return message;
     },
 
     singleUpload: (root, { file }, { processUpload }) => processUpload(file),
-    multipleUpload: (root, { files }, { processUpload }) => Promise.all(files.map(processUpload)),
-
+    multipleUpload: (root, { files }, { processUpload }) => Promise.all(files.map(processUpload))
   },
 
   Subscription: {
     mySub: {
-      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('hey'),
+      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('hey')
     },
     counter: {
       subscribe: (parent, args, { pubsub }) => {
-        const channel = Math.random().toString(36).substring(2, 15) // random channel name
-        let count = 0
-        setInterval(() => pubsub.publish(
-          channel,
-          {
-            // eslint-disable-next-line no-plusplus
-            counter: { count: count++ },
-          }
-        ), 2000)
-        return pubsub.asyncIterator(channel)
-      },
+        const channel = Math.random()
+          .toString(36)
+          .substring(2, 15); // random channel name
+        let count = 0;
+        setInterval(
+          () =>
+            pubsub.publish(channel, {
+              // eslint-disable-next-line no-plusplus
+              counter: { count: count++ }
+            }),
+          2000
+        );
+        return pubsub.asyncIterator(channel);
+      }
     },
 
     messageAdded: {
-      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('messages'),
-    },
-
-  },
-}
+      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('messages')
+    }
+  }
+};
--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
 // Load .env files
-const { loadEnv } = require('vue-cli-plugin-apollo/utils/load-env')
+const {
+  loadEnv
+} = require('vue-cli-plugin-apollo/utils/load-env')
 const env = loadEnv([
   path.resolve(__dirname, '.env'),
   path.resolve(__dirname, '.env.local')
