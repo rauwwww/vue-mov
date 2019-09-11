7c25b9b88fcfc5b8645a75ae0711009dc2c01756
Someone <Patrickholsths@gmail.com>
2019-09-11 20:16:06 +0200

    refactor: moved news header to own comp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,7 @@
 <template>
   <div>
     <h1 class="m-t-lg m-b-lg">Home</h1>
-    <vs-row vs-justify="center">
-      <vs-col class="p-l-lg p-r-lg" type="flex" vs-justify="center" vs-align="center" vs-w="4">
-        <vs-card actionable class="cardx" fixedHeight>
-          <div slot="header">
-            <h3>Lorem ipsum</h3>
-          </div>
-          <div slot="media">
-            <img src="@/assets/images/card.png" />
-          </div>
-          <div>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
-          </div>
-          <div slot="footer">
-            <vs-row vs-justify="flex-end">
-              <vs-button color="primary" type="gradient">View</vs-button>
-            </vs-row>
-          </div>
-        </vs-card>
-      </vs-col>
-      <vs-col class="p-r-lg" type="flex" vs-justify="center" vs-align="center" vs-w="4">
-        <vs-card actionable class="cardx" fixedHeight>
-          <div slot="header">
-            <h3>Lorem ipsum</h3>
-          </div>
-          <div slot="media">
-            <img src="@/assets/images/card.png" />
-          </div>
-          <div>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
-          </div>
-          <div slot="footer">
-            <vs-row vs-justify="flex-end">
-              <vs-button color="primary" type="gradient">View</vs-button>
-            </vs-row>
-          </div>
-        </vs-card>
-      </vs-col>
-      <vs-col class="p-r-lg" type="flex" vs-justify="center" vs-align="center" vs-w="4">
-        <vs-card actionable class="cardx" fixedHeight>
-          <div slot="header">
-            <h3>Lorem ipsum!</h3>
-          </div>
-          <div slot="media">
-            <img src="@/assets/images/card.png" />
-          </div>
-          <div>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
-          </div>
-          <div slot="footer">
-            <vs-row vs-justify="flex-end">
-              <vs-button color="primary" type="gradient">View</vs-button>
-            </vs-row>
-          </div>
-        </vs-card>
-      </vs-col>
-    </vs-row>
-
+    <NewsHeader />
     <vs-button class="m-t-lg m-r-lg" color="primary" type="filled">SomeCTA</vs-button>
   </div>
 </template>
@@ -65,10 +9,13 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import NewsHeader from '../components/NewsHeader.vue';
 
 @Component({
   name: 'Home',
-  components: {}
+  components: {
+    NewsHeader
+  }
 })
 export default class Home extends Vue {}
 </script>
